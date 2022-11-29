4ad1c2cb55ac849db21b18f14ecb61222f02c5b9
eternal17 <sarmadkhatri15@gmail.com>
2022-11-29 14:06:15 +0000

    final xp amount for all trnsacitions

--- script.js
+++ script.js
@@ -11,31 +11,58 @@ async function getApiData(graphqlQuery) {
   return response.json()
 }
 
-
-// for getting basic user details, username
-getApiData(`query {
-  user (where: {id: {_eq: 171}}) {
+// holds all of the xp transactions
+const arrayOfTransactionObjs = []
+// for getting xp points from div01
+getApiData(`{
+  user(where: {login: {_eq: "eternal17"}}) {
     login
+    id
+    transactions(
+      where: {_or: [{_and: [{amount: {_gt: 4999}}, {type: {_eq: "xp"}}, {object: {type: {_eq: "project"}}}]}, 
+        						{_and: [{path: {_iregex: "check-point"}}, {type: {_eq: "xp"}}]}, 
+        						{_and: [{type: {_eq: "xp"}}, {object: {type: {_eq: "piscine"}}}]}]}
+      order_by: {createdAt: desc}
+    ) {
+      amount
+      type
+      object {
+        name
+      }
+    }
   }
 }`)
   .then((data) => {
-    // username
-    console.log(data.data.user[0].login);
-    // userInfoDiv.innerHTML = data.data.user[0].login
+
+    // All of the transactions from the query above
+    const allTransactions = data.data.user[0].transactions
+
+    // creating new array and adding only project name and xp
+    allTransactions.forEach(obj => {
+      const objToPush = {}
+      objToPush["project"] = obj.object.name
+      objToPush["amount"] = obj.amount
+      arrayOfTransactionObjs.push(objToPush)
+    });
+
+    // removing all duplicates, only keeping highest xp for each project. (Some xp have been added to same project. Could be for bonus points etc. Do not add those.)
+    const uniqueTransacations = Object.values(arrayOfTransactionObjs.reduce((acc, obj) => {
+      const curr = acc[obj.project];
+      acc[obj.project] = curr ? (curr.amount < obj.amount ? obj : curr) : obj;
+      return acc;
+    }, {}));
+
+    // get all the xp points added up to display final xp figure, in KB. Same as on intra.
+
+    const finalXpAmount = uniqueTransacations.reduce((acc, obj) => {
+      return acc + obj.amount / 1000
+    }, 0)
+
+    const xpConvertedToKB = Math.round(finalXpAmount)
+    // display final xp amount
+
+
   })
 
 
 
-
-
-
-  // `    query {
-  //   transaction (  where: {
-  //     _and: [{ type: { _eq: "xp" } }, { userId: { _eq: 171 } }]
-  //   }) {
-  //     userId
-  //     amount
-  //     type
-  //     createdAt
-  //   }
-  // }`
