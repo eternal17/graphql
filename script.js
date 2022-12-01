d09f11f0c0b5c8ebea13b16f81d6292d40e06bb3
eternal17 <sarmadkhatri15@gmail.com>
2022-12-01 18:12:02 +0000

    pie-chart done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let userInfoDiv = document.getElementById("user-info")
-
+const scrollableProjects = document.querySelector(".scroll-projects")
+const pieWedge = document.querySelector(".pie-wedge")
+const pieTextPercent = document.querySelector(".pie-text")
+const pieProjectName = document.querySelector(".pie-project-name")
 async function getApiData(graphqlQuery) {
   const response = await fetch('https://learn.01founders.co/api/graphql-engine/v1/graphql', {
     method: "POST",
@@ -11,7 +13,7 @@ async function getApiData(graphqlQuery) {
   return response.json()
 }
 
-// holds all of the xp transactions
+// holds all of the xp transactions of div01
 const arrayOfTransactionObjs = []
 // for getting xp points from div01
 getApiData(`{
@@ -46,15 +48,16 @@ getApiData(`{
     });
 
     // removing all duplicates, only keeping highest xp for each project. (Some xp have been added to same project. Could be for bonus points etc. Do not add those.)
-    const uniqueTransacations = Object.values(arrayOfTransactionObjs.reduce((acc, obj) => {
+    const uniqueTransactions = Object.values(arrayOfTransactionObjs.reduce((acc, obj) => {
       const curr = acc[obj.project];
       acc[obj.project] = curr ? (curr.amount < obj.amount ? obj : curr) : obj;
       return acc;
     }, {}));
 
-    // get all the xp points added up to display final xp figure, in KB. Same as on intra.
 
-    const finalXpAmount = uniqueTransacations.reduce((acc, obj) => {
+
+    // get all the xp points added up to display final xp figure, in KB. Same as on intra.
+    const finalXpAmount = uniqueTransactions.reduce((acc, obj) => {
       return acc + obj.amount / 1000
     }, 0)
 
@@ -67,5 +70,81 @@ getApiData(`{
     displayXpDiv.innerHTML = xpConvertedToKB
   })
 
+// xp points from projects only (not including piscine and exercise xp)
+const xpFromProjects = []
+
+// for xp points of only projects (no piscine or excercises)
+
+getApiData(`{
+  user(where:  {login: {_eq: "eternal17"}}) {
+    transactions(
+      where: {_and: [{amount: {_gt: 4999}}, {type: {_eq: "xp"}}, {object: {type: {_eq: "project"}}}]}
+      order_by: {createdAt: desc}
+    ) {
+      amount
+      type
+      createdAt
+      object {
+        name
+        type
+      }
+    }
+  }
+}`).then((data) => {
+
+  const projectTransactions = data.data.user[0].transactions
+  console.log(projectTransactions)
+
+  // put into object and take out all the duplicates
+  projectTransactions.forEach((transaction) => {
+    const objToPush = {}
+    objToPush["project"] = transaction.object.name
+    objToPush["amount"] = transaction.amount
+    xpFromProjects.push(objToPush)
+  })
+
+  // removing all duplicates, only keeping highest xp for each project. (Some xp have been added to same project. Could be for bonus points etc. Do not add those.)
+  const uniqueTransactions = Object.values(xpFromProjects.reduce((acc, obj) => {
+    const curr = acc[obj.project];
+    acc[obj.project] = curr ? (curr.amount < obj.amount ? obj : curr) : obj;
+    return acc;
+  }, {}));
+
+  // get total amount of xp
+  const finalXpAmount = uniqueTransactions.reduce((acc, obj) => {
+    return acc + obj.amount
+  }, 0)
+
+  console.log(finalXpAmount, "xp without pis");
+
+  // find out percentage of each project, add to uniquetransactions object
+  uniqueTransactions.forEach((transaction) => {
+    transaction["percentage"] = ((transaction.amount / finalXpAmount) * 100).toFixed(1)
+
+    // add div for each project into scrollable div
+    const projectdiv = document.createElement("div")
+    projectdiv.classList.add("each-project")
+    projectdiv.innerHTML = transaction.project
+    projectdiv.id = transaction.percentage
+    scrollableProjects.appendChild(projectdiv)
 
 
+    // onclick change percentage to currently clicked
+    projectdiv.onclick = (e) => {
+      pieWedge.style.strokeDasharray = `calc(${e.target.id} * 31.42/100) 31.42`
+      pieTextPercent.innerHTML = `${e.target.id}%`
+      pieProjectName.innerHTML = transaction.project
+
+    }
+  })
+
+})
+
+// console.log(xpFromProjects, "xp from projects");
+// const idOne = document.querySelector("#one")
+// let pieWedge = document.querySelector(".pie-wedge")
+// idOne.onclick = () => {
+//   pieWedge.style.strokeDasharray = "calc(50 * 31.42/100) 31.42"
+// }
+
+// get only projects and find out percentages of each project compared to overall xpAmount
